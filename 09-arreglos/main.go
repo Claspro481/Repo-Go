2ea7e8bb32be0ebdc9ccc197d4c7cfc017325e10
Jostyn <jostyniraheta@gmail.com>
2026-07-02 20:30:14 -0600

    Arreglos

---
+++ 09-arreglos/main.go
@@ -0,0 +1,17 @@
+package main 
+
+import(
+	"fmt"
+)
+
+func main(){
+	//hacemos nuevos arreglos 
+	var teams [3]string
+	teams[0] = "Liverpool"
+	teams[1] = "Psg"
+	teams[2] = "Barcelona"
+
+	fmt.Println(teams)
+
+}
+
